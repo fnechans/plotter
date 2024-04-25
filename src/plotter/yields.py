09cc5a727990a4e62dc4331746d3454ba039d366
arastogi <angira.rastogi@cern.ch>
2024-04-25 22:18:32 +0200

    Modifying the columns of the table, with PDG rounding

--- src/plotter/yields.py
+++ src/plotter/yields.py
@@ -1,4 +1,5 @@
 import ROOT
+from plotter import pdgRounding
 
 def print_yields(hdata, hlistMC, ystr):
     ystr += "Process\t\t\tN_Entries\t\tIntegral\t\tUnderflow\t\tOverflow\n"
@@ -14,12 +15,22 @@ def print_yields(hdata, hlistMC, ystr):
 
 def print_yields_tex(title, hdata, hlistMC, ystr):
     s1, s2, s3, s4 = "", "", "", ""
-    s1 = "\\documentclass{article}\n\\usepackage{array}\n\\usepackage{graphicx} % for \\resizebox\n\\begin{document}\n\\begin{table}[htbp]\n\\centering\n\\caption{"+title+"}\n\\resizebox{\\textwidth}{!}{%\n\\begin{tabular}{|c|c|c|c|c|}\n\\hline\nProcess & N\_Entries & Integral & Underflow & Overflow\\\\\n\\hline\n"
+    s1 = "\\documentclass{article}\n\\usepackage{array}\n\\usepackage{graphicx} % for \\resizebox\n\\begin{document}\n\\begin{table}[htbp]\n\\centering\n\\caption{"+title+"}\n\\resizebox{\\textwidth}{!}{%\n\\begin{tabular}{|c|c|c|c|c|}\n\\hline\nProcess & N\_Entries & Yield & Statistical unc. & Systematic unc.\\\\\n\\hline\n"
     hst_data = hdata.th
-    s2 = str(hdata.title)+"&"+str(hst_data.GetEntries())+"&"+str(hst_data.Integral())+"&"+str(hst_data.GetBinContent(0))+"&"+str(hst_data.GetBinContent(hst_data.GetNbinsX()+1))+"\\\\\n"
+    entries = hst_data.GetEntries()
+    err_entries = entries**0.5
+    integral = hst_data.Integral()+hst_data.GetBinContent(0)+hst_data.GetBinContent(hst_data.GetNbinsX()+1)
+    integral, err_entries = pdgRounding.pdgRound(integral, err_entries)
+
+    s2 = str(hdata.title)+"&"+str(entries)+"&"+str(integral)+"&"+str(err_entries)+"&NA\\\\\n"
     for i in range(0,len(hlistMC)):
         hMC = hlistMC[i].th
-        s3 += f"{hlistMC[i].title}&{hMC.GetEntries()}&{hMC.Integral()}&{hMC.GetBinContent(0)}&{hMC.GetBinContent(hst_data.GetNbinsX()+1)}\\\\\n"
+        entries = hMC.GetEntries()
+        err_entries = entries**0.5
+        integral = hMC.Integral()+hMC.GetBinContent(0)+hMC.GetBinContent(hMC.GetNbinsX()+1)
+        integral, err_entries = pdgRounding.pdgRound(integral, err_entries)
+
+        s3 += f"{hlistMC[i].title}&{entries}&{integral}&{err_entries}&NA\\\\\n"
         
     s4 = "\\hline\n\\end{tabular}%\n}\\label{tab:"+title+"_yields_table}\n\\end{table}\n\\end{document}"
 
